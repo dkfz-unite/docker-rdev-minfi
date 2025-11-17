2fbe7833fb18e91090172754af37a98691ecdc57
vulasau <valiantsin.ulasau@gmail.com>
2025-11-17 11:27:27 +0100

    Add R script to install Bioconductor packages

---
+++ src/install.R
@@ -0,0 +1,6 @@
+install.packages("BiocManager")
+BiocManager::install("minfi", ask = T)
+BiocManager::install("limma", ask = T)
+BiocManager::install("IlluminaHumanMethylation450kmanifest", ask = T)
+BiocManager::install("IlluminaHumanMethylationEPICmanifest", ask = T)
+BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = T)
